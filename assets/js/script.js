4723b2c52204265cbc02649069fb7bc9ca79e7dc
PaulineGrn <pauline.gernigon@orange.fr>
2019-12-02 09:56:35 +0100

    JSPartie2Ex2

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//Création d'une fonction
+function hey(){
+  //Création d'une variable pour ratacher l'élément dans la HTML
+  var variable=document.getElementById("field").value;
+  /*Création d'une boîte de dialogue pour être afficher lors 
+  d'un clique sur n'importe quelle touche du clavier*/
+  alert(variable)
+}
